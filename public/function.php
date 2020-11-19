349586e341d28f4c00420b0fd6453bc5e400b47c
Tuliany <tuliany@mail.com>
2020-11-19 14:24:01 +0100

    fix json

--- public/function.php
+++ public/function.php
@@ -1,9 +1,9 @@
 <?php declare(strict_types=1); 
 function printablePeople(array $person): string {
-  $result = "<i>{$person['results']}</i>";
- /*  if( $peeps['height'] > 150 ){
-    $result .= "<b>{peeps.['height']}</b>";
-  } */
+  $result = "<i>{$person['name']}</i>";
+ if( $person['height'] > 150 ){
+    $result .= "<b>{person.['height']}</b>";
+  } 
   return $result;
 }
 
