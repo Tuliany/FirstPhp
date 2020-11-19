2cc3547a4eb22f54f507c0d160230d6ecd3bf1c0
Tuliany <tuliany@mail.com>
2020-11-19 14:34:46 +0100

    $ fix

--- public/function.php
+++ public/function.php
@@ -2,10 +2,8 @@
 function printablePeople(array $person): string {
   $result = "<i>{$person['name']}</i>";
  if( $person['height'] > 150 ){
-    $result .= "<b>{person.['height']}</b>";
+    $result .= "<b> {$person['height']}</b>";
   } 
   return $result;
 }
-
-
 ?>
